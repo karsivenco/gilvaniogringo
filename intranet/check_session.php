eae2abc2c509212bc95af2b80353356d9cc1c09d
Gilvani, o gringo <161768100+karsivenco@users.noreply.github.com>
2025-07-31 15:08:31 -0300

    Delete intranet directory

--- intranet/check_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-// check_session.php
-
-// Este endpoint retorna um JSON indicando se há um usuário logado na sessão.
-session_start();
-header('Content-Type: application/json');
-echo json_encode([
-    'authenticated' => isset($_SESSION['usuario'])
-]);
