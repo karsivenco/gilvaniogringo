5c652817d3ef88d71e8d04e824a7573b1bd32ee3
Gilvani, o gringo <161768100+karsivenco@users.noreply.github.com>
2025-08-01 09:42:37 -0300

    Delete intranet directory

--- intranet/check_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-// check_session.php
-
-// Retorna um JSON simples indicando se há um usuário logado
-session_start();
-header('Content-Type: application/json');
-echo json_encode([
-    'authenticated' => isset($_SESSION['usuario'])
-]);
