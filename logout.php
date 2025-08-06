18398fd5f868f09a9679fbe989f4650710de65ca
Gilvani, o gringo <161768100+karsivenco@users.noreply.github.com>
2025-08-06 08:54:58 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,7 @@
+<?php
+session_start();
+session_unset();      // Limpa as variáveis de sessão
+session_destroy();    // Destroi a sessão
 
+header("Location: intranet.html"); // Redireciona para tela de login
+exit;
