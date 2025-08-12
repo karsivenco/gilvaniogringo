1c2d7f48e9ef7c479d47465568a53dcf5a277272
Gilvani, o gringo <161768100+karsivenco@users.noreply.github.com>
2025-08-12 15:03:34 -0300

    Create senhas.js

---
+++ senhas.js
@@ -0,0 +1,23 @@
+// senhas.js
+
+const usuarios = {
+  "grazielle.albuquerque": "Gringo1975",
+  "gabriel.amaral": "Gringo1975",
+  "karina.maia": "Gringo@20",
+  "gilvaniogringo": "Gringo10000",
+  "anne.mattos": "Gringo1975",
+  "tati.garcia": "Gringo1975",
+  "cristiano.santos": "Gringo1975",
+  "vitor.mello": "Gringo1975",
+};
+
+const perfis = {
+  "karina.maia": "Desenvolvedora Social Mídia",
+  "cristiano.santos": "Mídias Sociais",
+  "anne.mattos": "Comunicação",
+  "tati.garcia": "Comunicação",
+  "gabriel.amaral": "Jornalismo",
+  "grazielle.albuquerque": "Jornalismo",
+  "vitor.mello": "Comunicação",
+  "gilvaniogringo": "Vereador",
+};
