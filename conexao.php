74d37d73d64a14da94f0d4f6c8ffdbda2553cd9a
Gilvani, o gringo <161768100+karsivenco@users.noreply.github.com>
2025-08-13 11:08:18 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,15 +1,12 @@
 <?php
-$host = "gilvaniogringo.mysql.dbaas.com.br";  // seu servidor
-$db   = "gilvaniogringo";                     // seu banco de dados
-$user = "gilvaniogringo";                     // seu usuário
-$pass = "Gringo@20";                          // sua senha
+$host = "gilvaniogringo.mysql.dbaas.com.br";
+$db   = "gilvaniogringo";
+$user = "gilvaniogringo";
+$pass = "Gringo@20";
 
-// Criar conexão
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Verificar conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
-// echo "Conexão bem-sucedida!";
 ?>
