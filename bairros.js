f06e38a8284ee3b0cc271631eb908e42bdaa59be
Gilvani, o gringo <161768100+karsivenco@users.noreply.github.com>
2025-08-15 09:03:35 -0300

    Update and rename localidades.js to bairros.js

---
+++ bairros.js
@@ -0,0 +1,11 @@
+// bairros.js
+
+const bairros = [
+  "Centro Histórico","Floresta","Auxiliadora","Moinhos de Vento","Independência","Bom Fim","Rio Branco","Montserrat","Bela Vista",
+  "Farroupilha","Santana","Petrópolis","Santa Cecília","Jardim Botânico","Praia de Belas","Cidade Baixa","Menino Deus","Azenha",
+  "Farrapos","Humaitá","Navegantes","São Geraldo","Anchieta","São João","Santa Maria Goretti","Higienópolis","Boa Vista","Passo D'Areia",
+  "Jardim São Pedro","Jardim Floresta","Cristo Redentor","Jardim Lindóia","São Sebastião","Vila Ipiranga","Jardim Itu","Jardim Europa",
+  "Camaquã","Cavalhada","Nonoai","Teresópolis","Vila Nova","Vila Assunção","Tristeza","Vila Conceição","Pedra Redonda","Ipanema",
+  "Espírito Santo","Guarujá","Serraria","Hípica","Campo Novo","Jardim Isabel","Aberta dos Morros","Sétimo Céu",
+  "Sarandi","Rubem Berta","Passo das Pedras","Santa Rosa de Lima","Parque Santa Fé","Costa e Silva","Jardim Leopoldina","Jardim Itu"
+];
