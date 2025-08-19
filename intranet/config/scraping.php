420043c949cac2d880906fa9417793abc8eb2639
Gilvani, o gringo <161768100+karsivenco@users.noreply.github.com>
2025-08-19 09:44:50 -0300

    Delete intranet directory

--- intranet/config/scraping.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    "urlSGCE" => "https://sgce.uffs.edu.br",
-    "urlRuApi" => "https://zzumgq.deta.dev"
-];
--- intranet/resources/views/livewire/certificates/update-certificates.blade.php
+++
@@ -1,5 +0,0 @@
-<div style="padding-top: 2%">
-    <x-jet-button type="button" class="mt-2" wire:click="updateCertificates">
-        {{ __('Atualizar Certificados') }}
-    </x-jet-button>
-</div>
